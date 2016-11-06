50853e1715bf752355171e8cdabbd1e4d2e6669e
rdaruwala <rdaruwala@gmail.com>
2016-11-06 07:04:08 -0600

    Final presenting version

--- HuskieHackiOSVersion/ChooseExpertViewController.swift
+++ HuskieHackiOSVersion/ChooseExpertViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import Parse
 import Firebase
+import CoreData
 
 class ChooseExpertViewController: UIViewController {
     
     var user:PFUser!
-
+    let defaults = UserDefaults.standard
+    var numKey:Int?
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var expertButton1: UIButton!
     @IBOutlet weak var expertButton2: UIButton!
@@ -21,16 +23,22 @@ class ChooseExpertViewController: UIViewController {
     private lazy var channelRef: FIRDatabaseReference = FIRDatabase.database().reference().child("channels")
     @IBOutlet weak var waitingMessage: UILabel!
     var isDone:Bool?
+    var done:Bool?
     override func viewDidLoad() {
         super.viewDidLoad()
         FIRApp.configure()
         isDone = false
+        done = false
         FIRAuth.auth()?.signInAnonymously(completion: { (user, error) in
             if let err:Error = error {
                 print(err.localizedDescription)
                 return
             }
         })
+        numKey = defaults.object(forKey: "key") as? Int
+        numKey = numKey! + 1
+        defaults.set(numKey, forKey: "key")
+        defaults.synchronize()
         
         let configuration = Config()
         let parseConfiguration = ParseClientConfiguration(block: { (ParseMutableClientConfiguration) -> Void in
@@ -103,6 +111,7 @@ class ChooseExpertViewController: UIViewController {
                                         var check = objects![i].object(forKey: "readyTalk")
                                         check = true
                                         objects![i].setObject(check, forKey: "readyTalk")
+                                        objects![i].saveInBackground()
                                         self.isDone = true
                                         self.performSegue(withIdentifier: "toChat", sender: self)
                                         toShow = false
@@ -152,8 +161,29 @@ class ChooseExpertViewController: UIViewController {
                         while(j < expertises.count){
                             if(expertises[j] == p){
                                 self.isDone = true
-                                objects![i].setObject(true, forKey: "readyTalk")
-                                self.performSegue(withIdentifier: "toChat", sender: self)
+                                let toSave = objects![i] as! PFUser
+                                toSave.setObject(true, forKey: "readyTalk")
+                                toSave.setObject("YO", forKey: "NICE")
+                                toSave.saveInBackground()
+                                let newUser = PFUser()
+                                newUser.username = String(describing: self.numKey)
+                                newUser.password = "999"
+                                newUser.signUpInBackground {
+                                    (success, error) -> Void in
+                                    if let error = error {
+                                        if let errorString = (error as NSError).userInfo["error"] as? String {
+                                            NSLog(errorString);
+                                        }
+                                    } else {
+                                        // Hooray! Let them use the app now.
+                                        NSLog("Signed up!");
+                                        
+                                    }
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+                                    self.performSegue(withIdentifier: "toChat", sender: self)
+                                })
+                                
                             }
                             j += 1
                         }
@@ -171,13 +201,25 @@ class ChooseExpertViewController: UIViewController {
     
     func checkStatus(){
         let status = user.object(forKey: "readyTalk") as! Bool
-        if(status == true){
-            user.setObject(false, forKey: "readyTalk")
-            self.performSegue(withIdentifier: "toChat", sender: self)
-        }
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
-            self.checkStatus()
+            let query = PFQuery(className: "_User")
+            query.whereKey("username", equalTo: String(describing: self.numKey))
+            query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) -> Void in
+                if error == nil {
+                    if(objects?[0] != nil || self.user.object(forKey: "NICE") as? String == "YO"||status == true){
+                        self.user.setObject(false, forKey: "readyTalk")
+                        self.user.saveInBackground()
+                        self.done = true
+                        self.performSegue(withIdentifier: "toChat", sender: self)
+                    }
+                    if(!self.done!){
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+                        self.checkStatus()
+                    })
+                    }
+                }
+            }
+            
         })
     }
 
@@ -190,8 +232,8 @@ class ChooseExpertViewController: UIViewController {
         if(segue.identifier == "toChat" && PFUser.current() != nil){
             let destination = segue.destination as! ChatViewController
             destination.senderDisplayName = user.object(forKey: "username") as! String!
-            destination.channel = Channel(id: "1", name: "Channel2")
-            destination.channelRef = channelRef.child("Channel2")
+            destination.channel = Channel(id: "1", name: "Expert Chat")
+            destination.channelRef = channelRef.child("Channel" + String(describing: numKey))
             destination.senderId = FIRAuth.auth()?.currentUser?.uid
         }
     }
